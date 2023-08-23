108b0613dccf6043a923c67ea6b46d8723e0ee90
SeaWardDM <135543915+SeaWardDM@users.noreply.github.com>
2023-08-23 16:54:38 -0500

    Added Ball and Paddles

---
+++ PongUnity/Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float movementSpeed;
+    public Vector3 direction;
+    public float maxXposition;
+ 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.position += direction * movementSpeed * Time.deltaTime;
+    }
+
+    public void Bounce(Vector3 newDirection)
+
+    {
+        direction = newDirection;
+    }
+
+    public void checkBoundaries()
+    {
+
+    }
+}
+
